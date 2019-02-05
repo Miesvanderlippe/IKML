0077d3d64f0d42e3a3f3e1967f399d4bd46ec8a4
Mies van der Lippe <miesvanderlippe@gmail.com>
2019-02-05 10:08:09 +0100

    Initial setup

---
+++ opdracht.r
@@ -0,0 +1,38 @@
+#--------Template - Onderdeel 2 - Machine Learning - ikml (40 punten) -------------
+# Naam student: Mies van der Lippe
+# studienummer: s1096607
+
+
+# Gekozen dataset (website link). Beschrijving dataset
+
+# Gekozen dataset (website link). Beschrijving dataset
+# http://archive.ics.uci.edu/ml/datasets/Iris
+# Dataset over Iris bloemen, bevat kelkblad lengte, breedte, bloemblad lengte en breedte en de soort.
+
+# Inlezen dataset en nul-meeting (10 punten)
+# met nul meeting bedoelen we 
+# - scatterplots maken. histrogram plots
+# - dimensies van je data (aantal rijen, aantal kolommen)
+# - numerieke of categorische data
+# - missing values? 
+# - Bereik van de datapunten?
+
+
+
+
+# gekozen classifier of regressie model trainen (10 punten)
+
+
+
+
+# cross validatie of R squared (voor regressie) (10 punten)
+
+
+
+
+# visualisatie en conclusies (10 punten)
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Setup 
+
+Download the dataset; 
+
+```
+wget -O iris.csv https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data
+```
